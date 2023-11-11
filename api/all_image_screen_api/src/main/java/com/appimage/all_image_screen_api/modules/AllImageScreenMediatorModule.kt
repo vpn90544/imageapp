961156ca40dd5d2281dc29225843ef0af6278e1e
Viktor Popov <vincet1998@gmail.com>
2023-11-11 23:00:51 +0300

    add screen in mediators module

--- api/all_image_screen_api/src/main/java/com/appimage/all_image_screen_api/modules/AllImageScreenMediatorModule.kt
+++ api/all_image_screen_api/src/main/java/com/appimage/all_image_screen_api/modules/AllImageScreenMediatorModule.kt
@@ -1,19 +1,19 @@
-package com.appimage.like_image_screen_api.modules
+package com.appimage.all_image_screen_api.modules
 
+import com.appimage.all_image_screen_api.mediators.AllImageScreenMediator
 import com.appimage.core.di.mediators.MediatorsMap
 import com.appimage.core.di.mediators.getMediator
 import com.appimage.core.di.qualifiers.MediatorsQualifier
-import com.appimage.like_image_screen_api.mediators.LikeImageScreenMediator
 import dagger.Module
 import dagger.Provides
 
 @Module
-object LikeImageScreenModule {
+object AllImageScreenMediatorModule {
 
     @Provides
     fun provideMainScreenMediator(
         @MediatorsQualifier mediatorsMap: MediatorsMap
-    ): LikeImageScreenMediator {
+    ): AllImageScreenMediator {
         return mediatorsMap.getMediator()
     }
 }
--- app/src/main/java/com/appimage/appimage/di/modules/MediatorsModule.kt
+++ app/src/main/java/com/appimage/appimage/di/modules/MediatorsModule.kt
@@ -1,11 +1,15 @@
 package com.appimage.appimage.di.modules
 
+import com.appimage.all_image_screen_api.modules.AllImageScreenMediatorModule
+import com.appimage.like_image_screen_api.modules.LikeImageScreenMediatorModule
 import com.appimage.mainscreen.di.modules.MainScreenMediatorModule
 import dagger.Module
 
 @Module(
     includes = [
-        MainScreenMediatorModule::class
+        MainScreenMediatorModule::class,
+        AllImageScreenMediatorModule::class,
+        LikeImageScreenMediatorModule::class
     ]
 )
 interface MediatorsModule
