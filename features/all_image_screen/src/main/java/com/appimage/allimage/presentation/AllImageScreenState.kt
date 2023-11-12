db01412ba90ede2b97b438779132a71c8ed025d4
Viktor Popov <vincet1998@gmail.com>
2023-11-12 20:29:46 +0300

    storage done

--- features/all_image_screen/src/main/java/com/appimage/allimage/presentation/AllImageScreenState.kt
+++ features/all_image_screen/src/main/java/com/appimage/allimage/presentation/AllImageScreenState.kt
@@ -5,5 +5,8 @@ import com.appimage.core_ui.view.category.CategoryViewModel
 import com.appimage.utils.adapter.DelegateItem
 
 data class AllImageScreenState (
-    val list: List<DelegateItem> = listOf()
+    val list: List<DelegateItem> = listOf(),
+    val isRefresh: Boolean = false,
+    val isLoadingNewPage: Boolean = false,
+    val nextPageLoad:String? = null
 ): BaseUiState
