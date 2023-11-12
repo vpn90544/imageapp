433cd0193f4fc24341ca2978d8ce278384116321
Viktor Popov <vincet1998@gmail.com>
2023-11-12 16:40:19 +0300

    add custom view for image2

--- features/all_image_screen/src/main/java/com/appimage/allimage/data/dto/Characters.kt
+++ features/all_image_screen/src/main/java/com/appimage/allimage/data/dto/Characters.kt
@@ -1,42 +1,17 @@
 package com.appimage.allimage.data.dto
 
-import androidx.room.ColumnInfo
-import androidx.room.Embedded
-import androidx.room.Entity
-import androidx.room.ForeignKey
-import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
-import java.io.Serializable
 
 data class ImagesInfoPage (
     @SerializedName("info"    ) var info    : Info?              = null,
-    @SerializedName("results" ) var results : ArrayList<CharacterDomain>? = null
+    @SerializedName("results" ) var results : ArrayList<ImageInfo>? = null
 )
 data class Info (
-    @SerializedName("count" ) var count : Int?    = null,
-    @SerializedName("pages" ) var pages : Int?    = null,
     @SerializedName("next"  ) var next  : String? = null,
     @SerializedName("prev"  ) var prev  : String? = null
 )
-data class Origin (
-    @SerializedName("name" ) var name : String? = null,
-    @SerializedName("url"  ) var url  : String? = null
-)
-data class Location (
-    @SerializedName("name" ) var name : String? = null,
-    @SerializedName("url"  ) var url  : String? = null
-)
-data class CharacterDomain(
+
+data class ImageInfo(
     @SerializedName("id"       ) var id       : Int?              = null,
-    @SerializedName("name"     ) var name     : String?           = null,
-    @SerializedName("status"   ) var status   : String?           = null,
-    @SerializedName("species"  ) var species  : String?           = null,
-    @SerializedName("type"     ) var type     : String?           = null,
-    @SerializedName("gender"   ) var gender   : String?           = null,
-    @SerializedName("origin"   ) var origin   : Origin?           = Origin(),
-    @SerializedName("location" ) var location : Location?         = Location(),
-    @SerializedName("image"    ) var image    : String?           = null,
-    @SerializedName("episode"  ) var episode  : ArrayList<String> = arrayListOf(),
-    @SerializedName("url"      ) var url      : String?           = null,
-    @SerializedName("created"  ) var created  : String?           = null
+    @SerializedName("image"    ) var image    : String?           = null
 )
