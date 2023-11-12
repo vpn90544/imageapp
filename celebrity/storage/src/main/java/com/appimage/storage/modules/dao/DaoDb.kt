caadc8a71725edff12af0bee6afe1dcf0c9895fa
Viktor Popov <vincet1998@gmail.com>
2023-11-12 17:35:29 +0300

    add custom view for image2

--- celebrity/storage/src/main/java/com/appimage/storage/modules/dao/DaoDb.kt
+++ celebrity/storage/src/main/java/com/appimage/storage/modules/dao/DaoDb.kt
@@ -1,8 +1,26 @@
 package com.appimage.storage.modules.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.appimage.storage.modules.dto.EntityAllImagesDB
+import com.appimage.storage.modules.dto.EntityLikeImagesDB
 
 @Dao
 interface DaoDb {
 
+    @Insert(entity = EntityLikeImagesDB::class)
+    fun insertLikeImages(likeImage: EntityLikeImagesDB)
+
+    @Insert(entity = EntityAllImagesDB::class)
+    fun insertAllImages(allImages: List<EntityAllImagesDB>)
+
+    @Query("SELECT * FROM allImages")
+    fun getAllImages(): List<EntityAllImagesDB>
+    @Query("SELECT * FROM likeImages")
+    fun getLikeImages():List<EntityLikeImagesDB>
+    @Query("DELETE FROM allImages")
+    fun clearAllImages()
+    @Query("DELETE FROM likeImages WHERE idImage =:idImage")
+    fun delItemFromLikeImages(idImage: Int)
 }
